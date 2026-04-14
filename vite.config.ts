8daf99c80aa7f2bb485b535ca4f59fa19734eedb
FITNESS_YASH <sarabjit.rattan@gmail.com>
2026-04-14 22:36:26 +0530

    feat: add resume page to sitemap and remove automated sitemap plugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import Sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -12,10 +11,6 @@ export default defineConfig(() => {
     },
     plugins: [
       react(),
-      Sitemap({
-        hostname: "https://sarabjeetrattan.com",
-        dynamicRoutes: ["/"],
-      }),
     ].filter(Boolean),
     resolve: {
       alias: {
